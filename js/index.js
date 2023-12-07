2e4fd93c6704a9214be80ec43b17e130e6f8097d
Dafarisy <dafarisy@gmail.com>
2023-12-07 20:40:37 +0700

    second push

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
-function hitungLuas(alas, tinggi) {
-    return 0.5 * alas * tinggi;
+function hitung() {
+    var alas = document.getElementById("alas").value;
+    var tinggi = document.getElementById("tinggi").value;
+   
+    var luas = (alas * tinggi) / 2;
+
+    alert("Luas segitiga: " + luas + "\nKeliling segitiga: " + keliling);
 }
 
-// Contoh penggunaan:
-var alas = 5;
-var tinggi = 10;
-var luas = hitungLuas(alas, tinggi);
-document.getElementById('hasil').innerHTML = "Luas segitiga: " + luas;
+function calculate() {
+    var sisi1 = document.getElementById("sisi1").value;
+    var sisi2 = document.getElementById("sisi2").value;
+    var sisi3 = document.getElementById("sisi3").value;
+
+    var keliling = parseInt(sisi1) + parseInt(sisi2) + parseInt(sisi3);
+
+    alert("Luas segitiga: " + luas + "\nKeliling segitiga: " + keliling);
+
+}
