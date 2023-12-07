ec6c15da231f6f96c08b37f32c7c206a6f4e2b3b
Dafarisy <dafarisy@gmail.com>
2023-12-07 22:11:08 +0700

    third push

--- js/index.js
+++ js/index.js
@@ -1,19 +1,12 @@
 function hitung() {
-    var alas = document.getElementById("alas").value;
-    var tinggi = document.getElementById("tinggi").value;
-   
-    var luas = (alas * tinggi) / 2;
+const alas = document.getElementById('alas')
+const tinggi = document.getElementById('tinggi')
+const hitung = document.getElementById('hitung')
+const output = document.getElementById('output')
+hitung.addEventListener('click', function(){
+    let a = alas.value
+    let t = tinggi.value
+    let l = 0.5*a*t
+    output.innerHTML = `Luas segitiga dari alas ${a} cm dan tinggi ${t} cm adalah ${l} cm2 `
 
-    alert("Luas segitiga: " + luas + "\nKeliling segitiga: " + keliling);
-}
-
-function calculate() {
-    var sisi1 = document.getElementById("sisi1").value;
-    var sisi2 = document.getElementById("sisi2").value;
-    var sisi3 = document.getElementById("sisi3").value;
-
-    var keliling = parseInt(sisi1) + parseInt(sisi2) + parseInt(sisi3);
-
-    alert("Luas segitiga: " + luas + "\nKeliling segitiga: " + keliling);
-
-}
+})}
